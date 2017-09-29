0a21e59b456f08a66c672c51ce80ccac77bf576a
listyanipraptiningputri <31992440+listyanipraptiningputri@users.noreply.github.com>
2017-09-29 23:35:10 +0700

    Delete query.php

--- query.php
+++
@@ -1,8 +0,0 @@
-<html>
-<title>Query</title>
-<body>
-<form enctype="multipart/form-data" method="POST" action="hasilquery.php">
-Keyword : <br>
-<input type="text" name="katakunci"><br>
-<input type=submit>
-</form>
